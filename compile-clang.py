669150e7222632c6f62ca5255a8180de3d500b17
Stephen Ostapenko <stepanos2002@gmail.com>
2022-03-14 02:13:12 +0300

    added comments

--- compile-clang.py
+++ compile-clang.py
@@ -7,6 +7,7 @@ import shutil
 import subprocess
 import time
 
+# Folder with LLVM project and git branch with actual version
 LLVM_PROJECT_PATH = "llvm-project"
 ACTUAL_GIT_BRANCH = "release/13.x"
 
@@ -20,6 +21,7 @@ def get_args():
 
 	return parser.parse_args()
 
+# Getting cmake configuration prompt
 def get_cmake_prompt_with_compilers(version, c_compiler_path, cxx_compiler_path):
 	result = f"cmake -S llvm -B build-v{version} -G \"Unix Makefiles\" -DLLVM_ENABLE_PROJECTS=\"clang\" -DCMAKE_BUILD_TYPE=Release"
 
@@ -34,6 +36,7 @@ def get_cmake_prompt_with_compilers(version, c_compiler_path, cxx_compiler_path)
 def get_cmake_prompt(version):
 	return get_cmake_prompt_with_compilers(version, "", "")
 
+# Latest available version number is stored in the following file
 LATEST_VERSION_FILE_NAME = ".clang-from-sources-latest-available-version"
 
 def write_latest_version_to_file(version):
@@ -50,6 +53,7 @@ def get_latest_version_from_file():
 	fin.close()
 	return version
 
+# Building completely new versions of clang from sources
 def build_from_scratch(thread_count, bootstrap_count):
 	# Doing cleanup before start
 	if (os.path.exists(LATEST_VERSION_FILE_NAME)):
@@ -61,14 +65,16 @@ def build_from_scratch(thread_count, bootstrap_count):
 
 	build_versions(1, bootstrap_count, thread_count)
 
+# Building some new versions using the latest available version of clang
 def build_with_addition(thread_count, add_bootstrap_count):
 	cur_version = get_latest_version_from_file()
 	build_versions(cur_version + 1, cur_version + add_bootstrap_count, thread_count)
 
-def build_versions(first, last, thread_count):
-	for version in range(first, last + 1):
+def build_versions(from_version, to_version, thread_count):
+	for version in range(from_version, to_version + 1):
 		start = time.time()
 
+		# Getting right prompt for cmake
 		prompt = ""
 		if (version == 1):
 			prompt = get_cmake_prompt(1)
@@ -76,8 +82,10 @@ def build_versions(first, last, thread_count):
 			compiler_path = f"{os.getcwd()}/build-v{version - 1}/bin/"
 			prompt = get_cmake_prompt_with_compilers(version, compiler_path + "clang", compiler_path + "clang++")
 		
+		# Configuring cmake
 		subprocess.check_call(prompt, shell = True)
 
+		# Building clang
 		os.chdir(f"build-v{version}")
 		subprocess.check_call(f"make clang -j {thread_count}", shell = True)
 		os.chdir("..")
@@ -88,6 +96,7 @@ def build_versions(first, last, thread_count):
 		print(f"\n\n\tVersion {version} was built in {end - start} s\n\n")
 
 if __name__ == "__main__":
+	# Parsing arguments
 	args = get_args()
 	thread_count = args.threads
 	bootstrap_count = args.steps

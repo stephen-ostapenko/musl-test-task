a2844e3b72672d0e06e4cf5a01233b098ea6d3ed
Stephen Ostapenko <stepanos2002@gmail.com>
2022-03-14 00:13:58 +0300

    implemented additional compilation

--- compile-clang.py
+++ compile-clang.py
@@ -7,11 +7,16 @@ import shutil
 import subprocess
 import time
 
+LLVM_PROJECT_PATH = "llvm-project"
+ACTUAL_GIT_BRANCH = "release/13.x"
+
 def get_args():
 	parser = argparse.ArgumentParser(description = "Script to compile clang from sources")
-
-	parser.add_argument("-n", "--steps", type = int, default = 1, help = "Bootstrap steps count (default: 1).")
 	parser.add_argument("-j", "--threads", type = int, default = 1, help = "Number of threads to work with (default: 1).")
+
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument("-n", "--steps", type = int, default = 1, help = "Initial bootstrap steps count (default: 1).")
+	group.add_argument("-a", "--add", type = int, help = "Additional bootstrap steps count.")
 
 	return parser.parse_args()
 
@@ -32,54 +37,76 @@ def get_cmake_prompt(version):
 LATEST_VERSION_FILE_NAME = ".clang-from-sources-latest-available-version"
 
 def write_latest_version_to_file(version):
-	out = open(LATEST_VERSION_FILE_NAME, "w")
-	out.write(str(version))
-	out.close()
+	fout = open(LATEST_VERSION_FILE_NAME, "w")
+	fout.write(f"{version}\n")
+	fout.close()
 
-LLVM_PROJECT_PATH = "llvm-project"
-ACTUAL_GIT_BRANCH = "release/13.x"
+def get_latest_version_from_file():
+	if (not os.path.exists(LATEST_VERSION_FILE_NAME)):
+		return 0
 
-args = get_args()
-bootstrap_count = args.steps
-thread_count = args.threads
+	fin = open(LATEST_VERSION_FILE_NAME, "r")
+	version = int(fin.read().split()[0])
+	fin.close()
+	return version
 
-# Downloading sources from github
-if (not os.path.exists(LLVM_PROJECT_PATH)):
-	subprocess.check_call("git clone https://github.com/llvm/llvm-project.git", shell = True)
+def build_from_scratch(thread_count, bootstrap_count):
+	# Doing cleanup before start
+	if (os.path.exists(LATEST_VERSION_FILE_NAME)):
+		os.remove(LATEST_VERSION_FILE_NAME)
 
-# Doing cleanup before start
-os.chdir(LLVM_PROJECT_PATH)
-if (os.path.exists(LATEST_VERSION_FILE_NAME)):
-	os.remove(LATEST_VERSION_FILE_NAME)
+	list_of_build_dirs = glob.glob("build-v*")
+	for d in list_of_build_dirs:
+		shutil.rmtree(d)
 
-list_of_build_dirs = glob.glob("build-v*")
-for d in list_of_build_dirs:
-	shutil.rmtree(d)
+	build_versions(1, bootstrap_count, thread_count)
 
-subprocess.check_call(f"git checkout {ACTUAL_GIT_BRANCH}", shell = True)
+def build_with_addition(thread_count, add_bootstrap_count):
+	cur_version = get_latest_version_from_file()
+	build_versions(cur_version + 1, cur_version + add_bootstrap_count, thread_count)
 
-# First compilation
-start = time.time()
+def build_versions(first, last, thread_count):
+	for version in range(first, last + 1):
+		start = time.time()
 
-subprocess.check_call(get_cmake_prompt(1), shell = True)
+		prompt = ""
+		if (version == 1):
+			prompt = get_cmake_prompt(1)
+		else:
+			compiler_path = f"{os.getcwd()}/build-v{version - 1}/bin/"
+			prompt = get_cmake_prompt_with_compilers(version, compiler_path + "clang", compiler_path + "clang++")
+		
+		subprocess.check_call(prompt, shell = True)
 
-os.chdir("build-v1")
-subprocess.check_call(f"make clang -j {thread_count}", shell = True)
-os.chdir("..")
+		os.chdir(f"build-v{version}")
+		subprocess.check_call(f"make clang -j {thread_count}", shell = True)
+		os.chdir("..")
 
-end = time.time()
-print(f"\n\n\tVersion 1 created in {end - start} s\n\n")
+		end = time.time()
 
-# Further compilations
-for version in range(2, bootstrap_count + 1):
-	start = time.time()
+		write_latest_version_to_file(version)
+		print(f"\n\n\tVersion {version} was created in {end - start} s\n\n")
 
-	compiler_path = f"{os.getcwd()}/build-v{version - 1}/bin/"
-	subprocess.check_call(get_cmake_prompt_with_compilers(version, compiler_path + "clang", compiler_path + "clang++"), shell = True)
+if __name__ == "__main__":
+	args = get_args()
+	thread_count = args.threads
+	bootstrap_count = args.steps
+	add_bootstrap_count = args.add
+	print(bootstrap_count, thread_count, add_bootstrap_count)
 
-	os.chdir(f"build-v{version}")
-	subprocess.check_call(f"make clang -j {thread_count}", shell = True)
-	os.chdir("..")
+	# Downloading sources from github
+	if (not os.path.exists(LLVM_PROJECT_PATH)):
+		subprocess.check_call("git clone https://github.com/llvm/llvm-project.git", shell = True)
 
-	end = time.time()
-	print(f"\n\n\tVersion {version} created in {end - start} s\n\n")
+	# Setting working directory
+	os.chdir(LLVM_PROJECT_PATH)
+	subprocess.check_call(f"git checkout {ACTUAL_GIT_BRANCH}", shell = True)
+
+	if (add_bootstrap_count == None):
+		print(f"\nBuilding {bootstrap_count} generations of clang from scratch\n")
+		build_from_scratch(thread_count, bootstrap_count)
+	else:
+		print(f"\nBuilding additional {add_bootstrap_count} generations of clang\n")
+		build_with_addition(thread_count, add_bootstrap_count)
+
+	print("\n\ndone!")
